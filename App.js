aa78ff16292f55ae20be27f77c089b9ebbb3cb18
Jim Pick <jim@backblend.com>
2018-03-21 21:39:23 -0700

    Add padding at top of react-native screen
    For status bar

--- App.js
+++ App.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import App from './src/components/App';
+import { View } from 'react-native';
+import { Constants } from 'expo';
 
 export default class extends React.Component {
   render() {
-    return <App />;
+    return (
+      <View style={{
+        paddingTop: Constants.statusBarHeight,
+        flex: 1
+      }}>
+        <App />
+      </View>
+    );
   }
 }
 
