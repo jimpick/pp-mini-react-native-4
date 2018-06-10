c494e254902fa0e380973eb39a4a4888f657e7f2
Jim Pick <jim@backblend.com>
2018-06-09 18:25:27 -0700

    Update modules, get electron version to work again
    Not sure how utp-native ever worked before.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path')
+const webpack = require('webpack')
 
 module.exports = {
+  mode: 'development',
   entry: ['babel-regenerator-runtime', './index.web.js'],
   output: {
     path: path.join(__dirname, 'desktop', 'dist'),
@@ -11,11 +13,52 @@ module.exports = {
     rules: [
       {
         test: /\.js$/,
-        exclude: /react-native-web/,
+        exclude: /node_modules/,
         loader: 'babel-loader',
       },
+      {
+        test: /\.node$/,
+        use: 'node-loader'
+      }
     ],
   },
+  plugins: [
+    // new webpack.ContextReplacementPlugin(/.*/, (context) => {
+    //   console.log('Jim', context)
+    //   if ( !/\/moment\//.test(context.context) ) return;
+    //
+    //  Object.assign(context, {
+    //    regExp: /^\.\/\w+/,
+    //    request: '../../locale' // resolved relatively
+    //  });
+    // })
+    /*
+    new webpack.ContextReplacementPlugin(/node-gyp-build/, (context) => {
+      console.log('Jim', context)
+      if (!/node-gyp-build/.test(context.context)) return
+    
+      Object.assign(context, {
+        request: '../utp-native/prebuilds/darwin-x64/electron-57.node'
+      })
+      console.log('Jim2', context)
+    })
+    */
+   /*
+    new webpack.ContextReplacementPlugin(
+      /node-gyp-build/,
+      path.resolve(__dirname, 'node_modules/utp-native/prebuilds/'),
+      true,
+      /\.node$/
+    )
+    */
+    new webpack.ContextReplacementPlugin(
+      /node-gyp-build/,
+      path.resolve(__dirname, 'node_modules/utp-native/prebuilds/'),
+      {
+        [path.resolve(__dirname, 'node_modules/utp-native/prebuilds/darwin-x64/electron-57.node')]: './darwin-x64/electron-57.node'
+      }
+    )
+  ],
   target: 'electron-renderer',
   resolve: {
     alias: {
